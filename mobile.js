d6f389cfbeba4b350b244f392aca83e264be7c62
kwoooriiii <169113809+kwoooriiii@users.noreply.github.com>
2024-05-07 12:26:33 +0800

    Update mobile.js

--- mobile.js
+++ mobile.js
@@ -87,3 +87,27 @@ papers.forEach(paper => {
   const p = new Paper();
   p.init(paper);
 });
+
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "I Wuff You, hehe!!";
+  gif.src =
+    "images/kiss.gif";
+});
+
+noBtn.addEventListener("click", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
